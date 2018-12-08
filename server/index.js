babfb1404d862e120f950ade889b44ee1cb42d29
Brandon Lyons <lyonsbp@umich.edu>
2018-12-08 01:52:38 -0500

    WORKING

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const dgram = require('dgram')
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const throttle = require('lodash/throttle')
 
 const port = 8889
 const host = '192.168.10.1'
@@ -11,10 +12,21 @@ drone.bind(port)
 const droneResponder = dgram.createSocket('udp4')
 droneResponder.bind(8890)
 
+function parseState (state) {
+  return state
+    .split(';')
+    .map(x => x.split(':'))
+    .reduce((data, [key, value]) => {
+      data[key] = value
+      return data
+    }, {})
+}
+
 io.on('connection', (socket) => {
   socket.on('command', command => {
     console.log('command received')
     console.log(command)
+    drone.send(command, 0, command.length, port, host)
   })
   console.log('connected')
 })
@@ -25,11 +37,11 @@ http.listen(3000, () => {
 
 droneResponder.on(
   'message',
-  state => {
-    // const formattedState = parseState(state.toString())
-    // io.sockets.emit('dronestate', formattedState)
-    console.log(state)
-  }
+  throttle(state => {
+    const formattedState = parseState(state.toString())
+    console.log(formattedState)
+    io.sockets.emit('dronestate', formattedState)
+  }, 500)
 )
 
 drone.send('command', 0, 'command'.length, port, host)
